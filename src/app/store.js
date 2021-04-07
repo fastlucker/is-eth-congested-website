b321aafee31edca5f8b0318ed107c3d3e4956134
Rangel Stoilov <rangelstoilov@gmail.com>
2021-04-07 13:28:15 +0300

    clean counter example slices

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
 //TODO: absolute imports
-import counterReducer from "../features/counter/counterSlice"
 import gasTrackerReducer from "../features/gasTracker/gasTrackerSlice"
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
     gasTracker: gasTrackerReducer,
   },
 })
