aa1f351a96e7aa08007331c7dc44c94cf479577e
Rangel Stoilov <rangelstoilov@gmail.com>
2021-04-07 13:17:53 +0300

    proxy cors request

--- src/features/gasTracker/gasTrackerAPI.js
+++ src/features/gasTracker/gasTrackerAPI.js
@@ -1,7 +1,18 @@
-// A mock function to mimic making an async request for data
-const fetchJSON = (input, init) => fetch(input, init).then(res => res.json())
+// since there the ethgas.watch doesn't have Access-Control-Allow-Origin": "*"
+// or API keys in order to access this from the browser we need to use a cors proxy
+// some proxy options: https://nordicapis.com/10-free-to-use-cors-proxies/
+// or in the future this can be ran in house
+// Here is the ethGasWatch repo: https://github.com/wslyvh/ethgaswatch
 
 export function fetchGasInfo() {
-  return fetchJSON("https://ethgasstation.info/api/ethgasAPI.json")
+  return fetch(
+    `https://api.allorigins.win/get?url=${encodeURIComponent(
+      "https://ethgas.watch/api/gas"
+    )}`
+  )
+    .then(response => {
+      if (response.ok) return response.json()
+      throw new Error("Network response was not ok.")
+    })
+    .then(data => JSON.parse(data.contents))
 }
-	
